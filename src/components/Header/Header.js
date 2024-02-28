e4080ac135323636e6b2a394ffb6759485bedb16
Leocoelho19822 <leo.coelho@ua.pt>
2024-02-28 21:15:02 +0000

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,8 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <h1>Meu Header</h1>
+      <h1>O Rio de Janeiro continua lindo</h1>
+      <h2>Um dos melhores cafés do mundo!</h2>
     </div>
   );
 }
