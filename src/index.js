712945c2f7bf0031c8c55e7d0f2e0b40af7af315
Leocoelho19822 <leo.coelho@ua.pt>
2024-03-06 17:40:09 +0000

    Removido arquivos desnecessarios

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Auth0Provider } from "@auth0/auth0-react";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-isntwb0ts5h00trx.us.auth0.com"
-      clientId="GCzmGRz9gWD8DITj7ryhc1Oj1P6G03Ws"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
+    
       <App />
-    </Auth0Provider>
+    
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
