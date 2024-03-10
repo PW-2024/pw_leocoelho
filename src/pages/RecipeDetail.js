65309d3547e8c9bc1c2377ee4ccc149ee78776de
Leocoelho19822 <leo.coelho@ua.pt>
2024-03-10 03:07:01 +0000

    update português

--- src/pages/RecipeDetail.js
+++ src/pages/RecipeDetail.js
@@ -17,18 +17,18 @@ export default function RecipeDetail() {
             <h1 className="text-3xl font-bold text-gray-800 mb-6">{recipe.nome}</h1>
             <div className="flex flex-wrap justify-around w-full md:w-2/3">
                 <div className="flex-1">
-                    <h2 className="text-2xl font-semibold text-gray-700 mb-4">Ingredientes</h2>
+                    <h2 className="text-2xl font-semibold text-gray-700 m-4">Ingredientes</h2>
                     <ul className="list-disc pl-5">
                         {ingredientes && ingredientes.conteudo.map((item, index) => (
-                            <li key={index} className="text-gray-600">{item}</li>
+                            <li key={index} className="text-gray-600 m-4">{item}</li>
                         ))}
                     </ul>
                 </div>
                 <div className="flex-1">
-                    <h2 className="text-2xl font-semibold text-gray-700 mb-4">Modo de Preparo</h2>
+                    <h2 className="text-2xl font-semibold text-gray-700 m-4">Modo de Preparo</h2>
                     <ul className="list-disc pl-5">
                         {modoDePreparo && modoDePreparo.conteudo.map((item, index) => (
-                            <li key={index} className="text-gray-600">{item}</li>
+                            <li key={index} className="text-gray-600 m-4">{item}</li>
                         ))}
                     </ul>
                 </div>
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Recipes({ recipes }) {
   return (
     <div className="flex flex-col items-center justify-center p-8">
-      <h1 className="text-4xl font-bold text-gray-800 mb-6">Choose a Recipe</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-6">Escolha uma receita</h1>
       <div className="flex gap-4 flex-wrap justify-center">
         {recipes.map((recipe) => (
           <Link
